813b543bead750ddb80d4349a060e108beee7da3
Rafael Cintra <ra.cintra@gmail.com>
2023-09-03 14:01:50 +0000

    Component Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatListModule } from '@angular/material/list';
+import { HeaderComponent } from './components/header/header.component';
 
 
 const materialModules = [
@@ -27,7 +28,8 @@ const materialModules = [
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
